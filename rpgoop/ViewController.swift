f5aafc175679fbf5700dbedb21b0620481b57d67
Juncheng Han <namrie1990@gmail.com>
2016-01-25 14:57:28 -0800

    Finished the functions

--- rpgoop/ViewController.swift
+++ rpgoop/ViewController.swift
@@ -10,16 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printLbl: UILabel!
+    @IBOutlet weak var playHpLbl: UILabel!
+    @IBOutlet weak var enemyHpLbl: UILabel!
+    @IBOutlet weak var enemyImg: UIImageView!
+    
+    @IBOutlet weak var cheseBtn: UIButton!
+    
+    var player: Player!
+    var enemy: Enemy!
+    var cheseMessege: String?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        player = Player(name: "DirtyLaundry", hp: 110, attackPwr: 20)
+        
+        generateRandomEnemy()
+        
+        playHpLbl.text = "\(player.hp) HP"
+    }
+    
+    func generateRandomEnemy() {
+        
+        let rand = Int(arc4random_uniform(2))
+        
+        if rand == 0 {
+            enemy = Kimara(startingHp: 50, attackPwr: 12)
+        } else {
+            enemy = DevilWizard(startingHp: 60, attackPwr: 16)
+        }
+        
+        enemyHpLbl.text = "\(enemy.hp) HP"
+        enemyImg.hidden = false
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+
+    @IBAction func onChaseBtnPressed(sender: AnyObject) {
+        cheseBtn.hidden = true
+        printLbl.text = cheseMessege
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "generateRandomEnemy", userInfo: nil, repeats: false)
     }
 
-
+    @IBAction func attackTapped(sender: AnyObject) {
+        
+        if enemy.attemptAttack(player.attackPwr) {
+            printLbl.text = "Attacked \(enemy.type) for \(player.attackPwr) HP"
+            enemyHpLbl.text = "\(enemy.hp) HP"
+        } else {
+            printLbl.text = "Attacked was unsuccessful!"
+        }
+        
+        if let loot = enemy.dropLoot() {
+            player.addItemToInventory(loot)
+            cheseMessege = "\(player.name) found \(loot)"
+            cheseBtn.hidden = false;
+        }
+        
+        if !enemy.isAlive {
+            enemyHpLbl.text = ""
+            printLbl.text = "Killed \(enemy.type)"
+            enemyImg.hidden = true
+        }
+    }
 }
 
